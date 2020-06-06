e872aec75672bd0ee83deca50885d480a4385d13
ahsansohail-n <ahsansohail.n@gmail.com>
2020-06-06 21:38:02 +0500

    Initial commit

---
+++ app/src/main/java/com/example/administrator/publiclibrary/Book_location.java
@@ -0,0 +1,24 @@
+package com.example.administrator.publiclibrary;
+
+/**
+ * Created by Administrator on 12/05/2017.
+ */
+
+public class Book_location {
+
+    public String booklocation;
+
+
+    public Book_location(String booklocation) {
+        this.booklocation = booklocation;
+    }
+
+    public Book_location()
+    {
+
+    }
+
+    public String getbooklocation() {
+        return booklocation;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/publiclibrary/locationlist.java
@@ -0,0 +1,26 @@
+package com.example.administrator.publiclibrary;
+
+/**
+ * Created by Administrator on 13/05/2017.
+ */
+
+public class locationlist {
+
+    public String loca;
+
+
+    public String getloca()
+    {
+        return loca;
+    }
+    public locationlist(String loca) {
+        this.loca = loca;
+    }
+
+    public  locationlist()
+    {
+
+    }
+
+
+}
